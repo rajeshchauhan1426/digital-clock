9eaf4d4e512eb83280df6fd59c25e7f3457348b4
Rajesh Chauhan <89766962+rajeshchauhan1426@users.noreply.github.com>
2023-09-23 19:24:18 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function updateTime() {
+    const timeElement = document.getElementById("time");
+    const now = new Date();
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+    const currentTime = `${hours}:${minutes}:${seconds}`;
+    timeElement.textContent = currentTime;
+}
+
+setInterval(updateTime, 1000); // Update the time every second
+updateTime(); // Call initially to set the initial time
